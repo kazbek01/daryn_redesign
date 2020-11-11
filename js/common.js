e085dbe10900aa9c0a1344b78d77a590693fd458
kazbek01 <sh.kazbek0603@gmail.com>
2020-11-11 11:25:31 +0600

    main-page

--- js/common.js
+++ js/common.js
@@ -15,13 +15,66 @@ $(document).ready(function () {
         $(this).removeClass('overlay-showed');
         $(".searchCover").removeClass('search-active');
         $('.modalBox').removeClass('showed');
-    })
+    });
+
     $('.close-modal').click(function () {
         $('.overlay').removeClass('overlay-showed');
         $('.modal-box').removeClass('showed');
         $('.modalBox').removeClass('showed');
-    })
+    });
 
+    $('.call-faq').click(function(){
+        $('.faq-fixed').addClass('show');
+    });
+
+    $('.close-faq').click(function(){
+        $('.faq-fixed').removeClass('show');
+    });
+    // var wow = new WOW({
+    //     animateClass: 'animated',
+    //     offset: 100
+    // });
+    // wow.init();
+
+
+    // //Auto complete
+    // var options = {
+    //     data: [{name: "Алгебра 8 сынып"},
+    //         {name: "Геометрия 8 сынып"},
+    //         {name: "Химия 8 сынып"}],
+    //
+    //
+    //     getValue: "name",
+    //
+    //     template: {
+    //         type: "custom",
+    //         method: function (value, item) {
+    //             return `
+    //                 <a href="">
+    //                     <div class="search-item">
+    //                         <div class="search-item-img">
+    //                             <img src="img/main/math.png" alt="">
+    //                         </div>
+    //                         <div class="search-item-caption">
+    //                             <h4>${value}</h4>
+    //                             <div class="rate-list">
+    //                                 <img src="img/icon/star-orange.svg" alt="">
+    //                                 <img src="img/icon/star-orange.svg" alt="">
+    //                                 <img src="img/icon/star-orange.svg" alt="">
+    //                                 <img src="img/icon/star-orange.svg" alt="">
+    //                                 <img src="img/icon/star-grey-br.svg" alt="">
+    //                             </div>
+    //                         </div>
+    //                         <span>таңдау</span>
+    //                     </div>
+    //                 </a>
+    //              `;
+    //         }
+    //     }
+    // };
+    // //Auto complete
+    //
+    // $(".search-complete").easyAutocomplete(options);
 
     $('.clear-search').click(function () {
         var $input = $(".search-complete").val();
